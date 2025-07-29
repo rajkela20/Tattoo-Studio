b44377becdfa368fce5c2278fa77d003ddd0a176
Rajix <dusanrajic111@gmail.com>
2025-07-29 23:03:56 +0300

    Secure Firebase config - moved keys to .env

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC_0owuXc-s6bbMRF4LbAVY43e7I4iLJFM",
-  authDomain: "tattoostudio-b07ca.firebaseapp.com",
-  projectId: "tattoostudio-b07ca",
-  storageBucket: "tattoostudio-b07ca.firebasestorage.app",
-  messagingSenderId: "441404689998",
-  appId: "1:441404689998:web:4434528bdfe92d0e48bd1f",
-  measurementId: "G-ZKWHB6D3XB"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
